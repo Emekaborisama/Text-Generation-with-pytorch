5f30a0de62d1beeac5a22b53ed254cee20517c33
emekaborisama <borisphiloosphy@gmail.com>
2021-03-03 14:22:40 +0100

    push final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn
+from app.api import app
 
 if __name__ == '__main__':
-    uvicorn.run("app.api:app",host= '0.0.0.0',port = 8080, debug=True)
+    app.run(host= '0.0.0.0',port = 8080, debug=True)
